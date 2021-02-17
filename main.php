d114fa7ac1a12e3c901874cb01e9566e7034390d
Fabrice <fabriceassim@gmail.com>
2021-02-17 11:57:11 +0300

    Ajout de group

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ class Main
 	private $rooms;
 	private $displays;
 	private $org;
+	private $group;
 	public function __construct($rooms, $displays, $org){
 		$this->rooms = $rooms;
 		$this->displays = $displays;
