3e4504ad7a257e5e2c1d5153c0fc19483d7ba6f9
Fabrice <fabriceassim@gmail.com>
2021-02-17 13:26:18 +0300

    Ajout de toi

--- main.php
+++ main.php
@@ -6,12 +6,14 @@ class Main
 	private $displays;
 	private $org;
 	private $group;
-	private $moi,
-	public function __construct($rooms, $displays, $org,$group,$moi){
+	private $moi;
+	private $toi;
+	public function __construct($rooms, $displays, $org,$group,$moi, $toi){
 		$this->rooms = $rooms;
 		$this->displays = $displays;
 		$this->org = $org;
 		$this->group = $group;
 		$this->moi = $moi;
+		$this->toi = $toi;
 	}
 }
