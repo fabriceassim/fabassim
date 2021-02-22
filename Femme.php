4b39207f2d5f2eacc82b7edaebbf5b20d43e7d81
Fabrice <fabriceassim@gmail.com>
2021-02-22 15:40:15 +0300

    Ajout de fonctionnalité

--- Femme.php
+++ Femme.php
@@ -2,7 +2,15 @@
 
 class Femme
 {
+	private $femme;
+	public function __construct($femme){
+		$this->femme = $femme;
+	}
 	public function init(){
 		return 'femme';
+	}
+
+	public function set($femme){
+		$this->femme = $femme;
 	}
 }
