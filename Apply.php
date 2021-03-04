8770afa2a70024aaa4e098610c803baf3219eff2
Fabrice <fabriceassim@gmail.com>
2021-03-04 18:36:35 +0300

    Add file Apply.php

---
+++ Apply.php
@@ -0,0 +1,21 @@
+<?php
+
+class Apply
+{
+	private $apply;
+	public function __construct($apply){
+		$this->apply = $apply;
+	}
+	public function init(){
+		return 'apply';
+	}
+
+	public function set($apply){
+		$this->apply = $apply;
+	}
+
+	public function dump(false)
+	{
+		return true;
+	}
+}
